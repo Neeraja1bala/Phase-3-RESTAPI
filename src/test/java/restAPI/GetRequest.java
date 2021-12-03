a4429d440161e5e293a96e012df0736321106a1f
Your Name <you@example.com>
2021-12-03 07:53:30 +0000

    API

---
+++ src/test/java/restAPI/GetRequest.java
@@ -0,0 +1,31 @@
+package restAPI;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class GetRequest {
+
+	
+	@Test
+	public void GetCall() {
+		
+		Logger logger = Logger.getLogger("APILogs");
+		PropertyConfigurator.configure("log4j.properties");
+		logger.setLevel(Level.DEBUG);
+		logger.info("********** GetCall ***********");
+		
+		RestAssured.given()
+		.baseUri("https://reqres.in/api")
+		.when()
+		.get("/users")
+		.then()
+		.statusCode(200)							//validation by code always added after then()
+		.log()
+		.all();
+	}
+}
